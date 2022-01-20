08524911e44e4f903357caa7ee0490242eab2d72
muhammadzain8 <muhammadzain8@gmail.com>
2022-01-20 17:17:05 +0500

    fix

--- src/hooks/useGaTracker.js
+++ src/hooks/useGaTracker.js
@@ -7,7 +7,7 @@ const useGaTracker = () => {
   const [initialized, setInitialized] = useState(false);
 
   useEffect(() => {
-    if (!location.href.includes('localhost')) {
+    if (!window.location.href.includes('localhost')) {
       ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_CODE);
     }
     setInitialized(true);
