737550e1085e0b8c609d21c0e8e71a9a550d6925
zain zani <muhammadzain8@gmail.com>
2022-03-01 22:36:56 +0500

    route fix

--- src/utils/makeReq.js
+++ src/utils/makeReq.js
@@ -2,19 +2,28 @@ import { LOCALSTORAGE_TOKEN_KEY } from 'contexts/AuthContext';
 import { toast } from 'react-toastify';
 
 // * Development URLs
+
 // const API_BASE_URL = `http://localhost:5000/api`;
 // const API_BASE_ORIGIN = `http://localhost:5000`;
+
 // const API_BASE_URL = `https://c9c6-119-73-118-95.ngrok.io/api`;
 // const API_BASE_ORIGIN = `https://c9c6-119-73-118-95.ngrok.io`;
 
 // * Production URLs
-const API_BASE_URL = `https://auction-api1.herokuapp.com/api`;
-const API_BASE_ORIGIN = `https://auction-api1.herokuapp.com`;
+//
+// const API_BASE_URL = `https://auction-api1.herokuapp.com/api`;
+// const API_BASE_ORIGIN = `https://auction-api1.herokuapp.com`;
+
+//*
+
+const API_BASE_URL = `http://lotpot.io:5000/api`;
+const API_BASE_ORIGIN = `http://:lotpot:5000`;
 
 const handleCatch = (err) => {
   let errMsg = 'Something Went Wrong';
   if (err.message) errMsg = err.message;
-  if (err.response?.data?.message) errMsg = err.response?.data?.message;
+  if (err.response?.data?.message)
+    errMsg = err.response?.data?.message;
   toast.error(errMsg);
 };
 
@@ -42,15 +51,17 @@ const makeReq = (
   if (body) {
     config.body = JSON.stringify(body);
   }
-  return fetch(`${API_BASE_URL}${endpoint}`, config).then(async (res) => {
-    const data = await res.json();
-    // console.log(`data`, data);
-    if (res.ok) {
-      return data;
-    } else {
-      return Promise.reject(data);
+  return fetch(`${API_BASE_URL}${endpoint}`, config).then(
+    async (res) => {
+      const data = await res.json();
+      // console.log(`data`, data);
+      if (res.ok) {
+        return data;
+      } else {
+        return Promise.reject(data);
+      }
     }
-  });
+  );
 };
 
 export { API_BASE_URL, API_BASE_ORIGIN, makeReq, handleCatch };
